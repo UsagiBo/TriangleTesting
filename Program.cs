f4f727490c8c5f5a3b5022654c0b19084f209766
rumi <g.e.usagi@gmail.com>
2017-02-28 20:34:12 +0100

    With UnitTests

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace TriangleTesting
 {
-    class Program
+    public class Program
     {
-        
+        static bool isit = false;
 
         public static void notZero(int a, int b ,int c)
         {
@@ -20,75 +20,110 @@ namespace TriangleTesting
                 Console.WriteLine("Sides of triangle can not be Null!");
             }
         }
+
         public static void equilateral(int a, int b, int c)
         {
             if (a==b && b==c && c == a)
             {
-                Console.WriteLine("This triangle is Equilateral!");
+                isit = true;
+                //Console.WriteLine("This triangle is Equilateral!");
             }
             else
             {
-                Console.WriteLine("This triangle is NOT Equilateral!");
+                isit = false;
             }
+            //else
+            //{
+            //    Console.WriteLine("This triangle is NOT Equilateral!");
+            //}
 
         }
 
         public static void isosceles(int a, int b, int c) {
             if (a==b || a==c || c == b)
             {
-                Console.WriteLine("This triangle is Isosceles!");
+                isit = true;
+                //Console.WriteLine("This triangle is Isosceles!");
             }
             else
             {
-                Console.WriteLine("This triangle is NOT Isosceles!");
+                isit = false;
             }
+            //else
+            //{
+            //    Console.WriteLine("This triangle is NOT Isosceles!");
+            //}
         }
 
         public static void scalene(int a, int b, int c)
         {
-            if(c>a && c > b)
+            if (c > a && c > b)
             {
                 int akv = a * a;
                 int bkv = b * b;
                 int ckv = c * c;
 
-                if (akv+bkv ==ckv)
+                if (akv + bkv == ckv)
                 {
-                    Console.WriteLine("This triangle is Scalene ");
+                    isit = true;
+                    //Console.WriteLine("This triangle is Scalene ");
 
                 }
                 else
                 {
-                    Console.WriteLine("This triangle is NOT Scalene ");
+                    isit = false;
                 }
-                if (a==b)
-                    Console.WriteLine("This triangle is Scalene and Equilateral");
+            //    else
+            //    {
+            //        Console.WriteLine("This triangle is NOT Scalene ");
+            //    }
+            //    if (a == b)
+            //        Console.WriteLine("This triangle is Scalene and Equilateral");
             }
-
-
         }
+
+            public static string WhatIsThisTriangle(int d,int e, int f)
+        {
+            equilateral(d, e, f);
+            if (isit == true)
+            {
+                return "equilateral";
+            }
+            isosceles(d, e, f);
+            if (isit == true)
+            {
+                return "isosceles";
+            }
+            scalene(d, e, f);
+            if (isit == true)
+            {
+                return "scalene";
+            }
+            else return "false";
+        }
+        
 
         static void Main(string[] args)
         {
-            string input;
-            int a;
-            int b;
-            int c;
-            Console.WriteLine("Please enter three values in the following order:");
-            Console.WriteLine("Enter value for side 'a': ");
-            input = Console.ReadLine();
-            a = Int32.Parse(input);
-            Console.WriteLine("Enter value for side 'b': ");
-            input = Console.ReadLine();
-            b = Int32.Parse(input);
-            Console.WriteLine("Enter value for side 'c': ");
-            input = Console.ReadLine();
-            c = Int32.Parse(input);
+            //string input;
+            //int a;
+            //int b;
+            //int c;
+            //Console.WriteLine("Please enter three values in the following order:");
+            //Console.WriteLine("Enter value for side 'a': ");
+            //input = Console.ReadLine();
+            //a = Int32.Parse(input);
+            //Console.WriteLine("Enter value for side 'b': ");
+            //input = Console.ReadLine();
+            //b = Int32.Parse(input);
+            //Console.WriteLine("Enter value for side 'c': ");
+            //input = Console.ReadLine();
+            //c = Int32.Parse(input);
 
-            notZero(a, b, c);
-            equilateral(a, b, c);
-            isosceles(a, b, c);
-            scalene(a, b, c);
+            //notZero(a, b, c);
+            //equilateral(a, b, c);
+            //isosceles(a, b, c);
+            //scalene(a, b, c);
 
 
 
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program.WhatIsThisTriangle(10, 10, 15);   
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Program.WhatIsThisTriangle(0 , 0 , 1);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Program.WhatIsThisTriangle(-10, 10, 15);
+        }
+    }
+}
